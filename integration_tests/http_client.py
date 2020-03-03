fb8c75dbb70bd0d7a7aa49b4e45454cba8f5fa51
ksamuel <kevin.samuel@yandex.com>
2020-03-03 12:48:42 +0100

    First integration tests for usershub

---
+++ integration_tests/http_client.py
@@ -0,0 +1,62 @@
+import requests
+
+import pytest
+from integration_tests_conf import TAXHUB_URL, USERSHUB_CONFIG
+
+
+class HttpClient:
+    def __init__(self, service_conf):
+        self.conf = service_conf
+        self.root = service_conf.url
+        self.session = requests.Session()
+
+    def join_url(self, start, end):
+        return start.rstrip("/") + "/" + end.lstrip("/")
+
+    def get(self, path, *args, **kwargs):
+        return self.session.get(self.join_url(self.root, path), *args, **kwargs)
+
+    def post(self, path, *args, **kwargs):
+        return self.session.post(self.join_url(self.root, path), *args, **kwargs)
+
+    def put(self, path, *args, **kwargs):
+        return self.session.put(self.join_url(self.root, path), *args, **kwargs)
+
+    def patch(self, path, *args, **kwargs):
+        return self.session.patch(self.join_url(self.root, path), *args, **kwargs)
+
+    def delete(self, path, *args, **kwargs):
+        return self.session.delete(self.join_url(self.root, path), *args, **kwargs)
+
+    def check_status(self, url, code, *args, **kwargs):
+        return self.get(url, *args, **kwargs).status_code == code
+
+    def login(self):
+        conf = self.conf
+        return self.post(
+            conf.login_url,
+            json={
+                "id_application": conf.application_id,
+                "login": conf.test_login,
+                "password": conf.test_login,
+            },
+        )
+
+    def __enter__(self):
+        self.session.__enter__()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.session.__exit__(*args, **kwargs)
+
+
+@pytest.fixture
+def usershub_client():
+    with HttpClient(USERSHUB_CONFIG) as client:
+        yield client
+
+
+@pytest.fixture
+def taxhub_client():
+    with HttpClient(root=TAXHUB_URL) as client:
+        yield client
--- integration_tests/integration_tests_conf.py
+++ integration_tests/integration_tests_conf.py
@@ -1,9 +1,18 @@
 import os
+from types import SimpleNamespace
 
-GEONATURE_URL = os.environ.get("GEONATURE_URL", "http://127.0.0.0:8000")
+GEONATURE_URL = SimpleNamespace(
+    url=os.environ.get("GEONATURE_URL", "http://127.0.0.0:8000"),
+    application_id=os.environ.get("GEONATURE_APPLICATION_ID", "3"),
+)
 
+TAXHUB_URL = os.environ.get("TAXHUB_URL", "http://127.0.0.1:5000")
 
-TAXUB_URL = os.environ.get("TAXUB_URL", "http://127.0.0.1:5000")
-
-
-USERSHUB_URL = os.environ.get("USERSHUB_URL", "http://127.0.0.1:5001")
+USERSHUB_CONFIG = SimpleNamespace(
+    url=os.environ.get("USERSHUB_URL", "http://127.0.0.1:5001"),
+    test_login=os.environ.get("USERSHUB_TEST_LOGIN", "admin"),
+    test_password=os.environ.get("USERSHUB_TEST_PASSWORD", "admin"),
+    login_url=os.environ.get("USERSHUB_LOGIN_URL", "/pypn/auth/login"),
+    application_id=os.environ.get("USERSHUB_APPLICATION_ID", "1"),
+    root_org_id=os.environ.get("USERSHUB_ROOT_ORG_ID", "0"),
+)
