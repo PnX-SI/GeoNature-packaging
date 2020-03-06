018aab33c0ac6f9cf7f0d44c55caff9659805e3c
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2020-03-06 11:46:14 +0100

    Ajout venv

--- create_deb_file_usershub.sh
+++ create_deb_file_usershub.sh
@@ -1,4 +1,15 @@
+# creation du venv
+python3 -m venv /tmp/venv
+source /tmp/venv/bin/activate
+pip install -r requirements.txt
+
+echo "Create usershub build"
 ./manage.py build usershub 0.0.1 1 --keep-temp-dir --taxhub-repo-uri /home/pnc-si/dev/TaxHub --usershub-repo-uri /home/pnc-si/dev/UsersHub --geonature-repo-uri ~/dev/GeoNature --usershub-checkout develop
 
+deactivate
+
+echo "Purge installation"
 sudo apt purge -y usershub
+
+echo "Installation"
 sudo apt install ./build/usershub_0.0.1-1_amd64.deb
